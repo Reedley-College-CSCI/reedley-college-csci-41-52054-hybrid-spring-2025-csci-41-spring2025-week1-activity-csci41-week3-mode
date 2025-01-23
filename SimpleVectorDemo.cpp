90a061f69fb000cb62a79ef0e668a29762b224f5
KalebSand <157537090+KalebSand@users.noreply.github.com>
2025-01-23 12:47:37 -0800

    Update SimpleVectorDemo.cpp
    Added a new simple vector called intTable2

--- SimpleVectorDemo.cpp
+++ SimpleVectorDemo.cpp
@@ -8,8 +8,11 @@ int main()
     const int SIZE = 10;    // Number of elements
     int count;              // Loop counter
 
-    // Create a SimpleVector of ints.
+    // Create the first SimpleVector of ints.
     SimpleVector<int> intTable(SIZE);
+
+    // Create the second SimpleVector of ints
+    SimpleVector<int> intTable2(SIZE);
 
     // Create a SimpleVector of doubles.
     SimpleVector<double> doubleTable(SIZE);
@@ -18,6 +21,8 @@ int main()
     for (count = 0; count < SIZE; count++)
     {
         intTable[count] = (count * 2);
+        // Multiplying by 4
+        intTable2[count] = (count * 4);
         doubleTable[count] = (count * 2.14);
     }
 
@@ -25,6 +30,11 @@ int main()
     cout << "These values are in intTable:\n";
     for (count = 0; count < SIZE; count++)
         cout << intTable[count] << " ";
+    cout << endl;
+    // Display the values in intTable2
+    cout << "These values are in intTable2:\n";
+    for (count = 0; count < SIZE; count++)
+        cout << intTable2[count] << " ";
     cout << endl;
     cout << "These values are in doubleTable:\n";
     for (count = 0; count < SIZE; count++)
