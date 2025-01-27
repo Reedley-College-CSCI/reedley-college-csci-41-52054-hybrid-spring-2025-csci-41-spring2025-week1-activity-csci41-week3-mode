e66dd6aa26a0c14e0b619250e62bb6c1b2f0f112
Marcelino Salazar <marcelinosalazar2@gmail.com>
2025-01-27 13:30:40 -0800

    test

--- SimpleVectorDemo.cpp
+++ SimpleVectorDemo.cpp
@@ -1,3 +1,6 @@
+// Marcelino Salazar
+// CSCI-41 Lab 1 Part 2
+
 // This program demonstrates the SimpleVector template.
 #include <iostream>
 #include "SimpleVector.h"
@@ -8,8 +11,10 @@ int main()
     const int SIZE = 10;    // Number of elements
     int count;              // Loop counter
 
-    // Create a SimpleVector of ints.
+    // Create a SimpleVector of int created new simple vector of ints.
     SimpleVector<int> intTable(SIZE);
+    SimpleVector<int> intTable2(10);
+    
 
     // Create a SimpleVector of doubles.
     SimpleVector<double> doubleTable(SIZE);
@@ -18,6 +23,7 @@ int main()
     for (count = 0; count < SIZE; count++)
     {
         intTable[count] = (count * 2);
+        intTable2[count] = (count * 3);
         doubleTable[count] = (count * 2.14);
     }
 
@@ -26,6 +32,12 @@ int main()
     for (count = 0; count < SIZE; count++)
         cout << intTable[count] << " ";
     cout << endl;
+
+    cout << "These values are in intTable2:\n";
+    for (count = 0; count < SIZE; count++)
+        cout << intTable2[count] << " ";
+    cout << endl;
+
     cout << "These values are in doubleTable:\n";
     for (count = 0; count < SIZE; count++)
         cout << doubleTable[count] << " ";
